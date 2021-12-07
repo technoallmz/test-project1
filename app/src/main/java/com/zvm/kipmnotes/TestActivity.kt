962835f9cb94c08d36559387dcb91b2b1753899d
Zakariya <mohdzakariya272@gmail.com>
2021-12-07 17:53:24 +0530

    removed print statements

--- app/src/main/java/com/zvm/kipmnotes/TestActivity.kt
+++ app/src/main/java/com/zvm/kipmnotes/TestActivity.kt
@@ -7,9 +7,6 @@ class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
-        print("testing...")
-        print("from test branch 2")
-        print("from test branch 1")
 
     }
 }
